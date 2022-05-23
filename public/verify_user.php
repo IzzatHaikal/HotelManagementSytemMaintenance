03ef17b1b100b20d038fef6c477ad47a9a33c2bb
IzzatHaikal <58548850+IzzatHaikal@users.noreply.github.com>
2022-05-24 02:29:32 +0800

    Heroku verify user config 4

--- public/verify_user.php
+++ public/verify_user.php
@@ -9,8 +9,7 @@
      $verification = isUserVerified($pdo, $_SESSION["user_email"]);
      $status["verified"] = true;
 
-     header("Location: /index.php");
-     exit();
+     header("Location: index.php");
      return;
      if ($verification["verified"] == 1) {
          $status["verified"] = true;
@@ -55,35 +54,3 @@
 ?>
 
 
-<?php
-include("./includes/header.php");
-?>
-<body>
-  <header id="verify-user">
-    <div id="verify-user--bg-image">
-      <?php include("./includes/navbar.php"); ?>
-      <div class="wrapper">
-      <div class="container">
-            <div class="content p-5">
-                <h2><b>Verify your account</b></h2>
-                <p>A verification link has been sent to your registered email id</p>
-                <?php if ($status["verified"]) { ?> 
-                  <h3 class="text-success ">Your account has been verified redirecting...</h3>
-                <?php } elseif ($status["verified"] == null) { ?> 
-                <p></p>
-                <?php } else {?> 
-                  <h3 class="text-danger">Invalid link...</h3>
-                <?php } ?>
-                <form action="./core/resend_mail.php" method="GET">
-                  <button class="btn btn-outline-primary" type="submit" name="resend" value="resend">Resend email</button>
-                </form>
-            </div>
-      </div>
-      </div>
-    </div>
-  </header>
-  <main>
-  </main>
-  <?php include("./includes/footer.php"); ?>
-</body>
-</html>
