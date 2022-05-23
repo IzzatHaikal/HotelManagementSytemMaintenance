3413df7540dbadb420e4dae46f3c8f8c26afddb1
IzzatHaikal <58548850+IzzatHaikal@users.noreply.github.com>
2022-05-24 00:57:23 +0800

    Heroku database config final

--- public/core/db.php
+++ public/core/db.php
@@ -1,19 +1,17 @@
 <?php
 
+    try {
+        $pdo =  new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        session_start();
+    } catch (PDOException $err) {
+        die($err->getMessage());
+    }
+    
     // try {
-    //     $pdo =  new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+    //     $pdo =  new PDO("mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_005a985cabfb7a3,b8005b16c70c6a,93b3cea1");
     //     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //     session_start();
     // } catch (PDOException $err) {
     //     die($err->getMessage());
     // }
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
