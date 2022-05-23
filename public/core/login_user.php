5a2018cf5545dff6b6c166731f054d4ccbec8cf6
IzzatHaikal <58548850+IzzatHaikal@users.noreply.github.com>
2022-05-24 01:52:55 +0800

    Heroku htaaccess config 12

--- public/core/login_user.php
+++ public/core/login_user.php
@@ -1,9 +1,6 @@
 <?php
 require("../config.php");
 header("Content-Type: application/json; charset=utf-8");
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
 
 $user_email=$_POST['user_email'];
