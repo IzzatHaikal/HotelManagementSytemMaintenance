aa32759ac385fb740644aba634a137e6bb94a34b
IzzatHaikal <58548850+IzzatHaikal@users.noreply.github.com>
2022-06-03 15:12:13 +0800

    localhost setting 2

--- public/core/login_user.php
+++ public/core/login_user.php
@@ -1,9 +1,9 @@
 <?php
 require("../config.php");
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+// header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+// header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 header("Content-Type: application/json; charset=utf-8");
 
 
